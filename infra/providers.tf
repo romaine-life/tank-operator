3767c860364d5582172b731355e5aa106a5b5230
Nelson Romaine <nelson@romaine.life>
2026-05-04 17:09:50 -0700

    Disable Azure provider registration in tank infra

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,17 +1,19 @@
 provider "azurerm" {
   features {}
-  use_oidc        = true
-  subscription_id = var.arm_subscription_id
-  tenant_id       = var.arm_tenant_id
+  use_oidc                        = true
+  subscription_id                 = var.arm_subscription_id
+  tenant_id                       = var.arm_tenant_id
+  resource_provider_registrations = "none"
 }
 
 provider "azurerm" {
   alias = "cluster"
 
   features {}
-  use_oidc        = true
-  subscription_id = var.cluster_subscription_id
-  tenant_id       = var.arm_tenant_id
+  use_oidc                        = true
+  subscription_id                 = var.cluster_subscription_id
+  tenant_id                       = var.arm_tenant_id
+  resource_provider_registrations = "none"
 }
 
 provider "azuread" {
